fa1f262f42055c83f8c05b553044628c28dee83a
Dewey Dunnington <dewey@fishandwhistle.net>
2019-04-09 23:26:45 -0300

    first commit

---
+++ R/data.R
@@ -0,0 +1,17 @@
+
+#' Alta Lake Gravity Core Data
+#'
+#' Physical properties and lead-210 activity information from Alta Lake, Whistler,
+#' British Columbia, Canada.
+#'
+#' @seealso \link[mudata2]{alta_lake}
+#'
+#' @references
+#' Dunnington DW, Spooner IS, White CE, et al (2016) A geochemical perspective on the impact of
+#' development at Alta Lake, British Columbia, Canada. J Paleolimnol 56:315-330.
+#' doi:10.1007/s10933-016-9919-x
+#'
+"alta_lake_210Pb"
+
+#' @rdname alta_lake_210Pb
+"alta_lake_drying"
---
+++ data-raw/collect_alta_lake.R
@@ -0,0 +1,19 @@
+
+library(tidyverse)
+
+alta_lake_drying <- readxl::read_excel("data-raw/alta_lake_pb210.xlsx", sheet = "drying")
+alta_lake_210Pb_raw <- readxl::read_excel("data-raw/alta_lake_pb210.xlsx", sheet = "pb210")
+
+alta_lake_210Pb <- alta_lake_210Pb_raw %>%
+  mutate(
+    sample_id = paste0("AL-GC2-", section_top_cm)
+  ) %>%
+  unite("date_counted", starts_with("date_counted"), sep = "-") %>%
+  unite("date_extracted", starts_with("date_extracted"), sep = "-") %>%
+  mutate_at(vars(starts_with("date_")), as.Date) %>%
+  select(sample_id, everything()) %>%
+  print()
+
+
+usethis::use_data(alta_lake_drying, overwrite = TRUE)
+usethis::use_data(alta_lake_210Pb, overwrite = TRUE)
