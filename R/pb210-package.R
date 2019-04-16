72c2e531c04b49f46cca1dc6a3e073ab3c186acf
Dewey Dunnington <dewey@fishandwhistle.net>
2019-04-16 16:28:57 -0300

    add error operations to reexports

--- R/pb210-package.R
+++ R/pb210-package.R
@@ -1,7 +1,18 @@
 #' @keywords internal
-#' @importFrom errors set_errors drop_errors errors
 "_PACKAGE"
 
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
+
+#' @importFrom errors drop_errors
+#' @export
+errors::drop_errors
+
+#' @importFrom errors set_errors
+#' @export
+errors::set_errors
+
+#' @importFrom errors errors
+#' @export
+errors::errors
